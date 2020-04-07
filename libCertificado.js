aed499022c65a981f023cc5a86865c30a6d8e770
Artur Diniz Adam <artdiniz@users.noreply.github.com>
2020-04-07 05:17:16 -0300

    Update libCertificado.js

--- libCertificado.js
+++ libCertificado.js
@@ -8,40 +8,27 @@ function emitirCertificado(nome = "") {
   
   if(contadorEmitir == 1) {
      contadorTimeout = setTimeout(function() {
-      alert(`
-        Achou que ia ser fácil?
-        Chame a função emitirCertificado() de novo lá no seu código!
-      `)
+      alert(`Achou que ia ser fácil?\nChame a função emitirCertificado() de novo lá no seu código!`)
        contadorEmitir = 0
     }, 1000)
   }
   else if(contadorEmitir == 2) {
     contadorTimeout = setTimeout(function() {
-      alert(`
-        Ainda não! 
-        Chame emitirCertificado() mais uma vez no seu código!
-      `)
+      alert(`Ainda não!\nChame emitirCertificado() mais uma vez no seu código!`)
        contadorEmitir = 0
     }, 1000)
   }
   else if(contadorEmitir < 10) {
     contadorTimeout = setTimeout(function() {
-      alert(`
-        Boa! Mas você precisa chamar essa funcão 1 milhão de vezes.
-        Você chamou já chamou a função ${contadorEmitir} ${
+      alert(`Boa! Mas você precisa chamar essa funcão 1 milhão de vezes.\nVocê chamou já chamou a função ${contadorEmitir} ${
           contadorEmitir == 1 ? 'vez' : 'vezes'
-        }. 
-        Faltam ${ numeroChamadas - contadorEmitir}!
-      `)
+        }.\nFaltam ${ numeroChamadas - contadorEmitir}!`)
        contadorEmitir = 0
     }, 1000)
   }
 
   if(contadorEmitir >= numeroChamadas) {
     contadorEmitir = 0
-    confirm(`
-      Parabéns${nome !== "" ? ' ' + nome : ''}! Você concluiu a #quarentenaDev =D
-      Continuar para o certificado?
-    `) 
+    confirm(`Parabéns${nome !== "" ? ' ' + nome : ''}!\nVocê concluiu a #quarentenaDev =D\nContinuar para o certificado?`) 
   }
 }
